40c077560f88f1ef093dc5dd0db8046352bdf778
Ansh Bhalala <abhalala@asu.edu>
2024-10-15 02:41:30 -0700

    Modified wezterm.lua for Qol,Modified init.lua to contain gui/neovide config,Removed example.lua

--- extras/wezterm.lua
+++ extras/wezterm.lua
@@ -2,47 +2,49 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font_with_fallback({
-	"FantasqueSansM Nerd Font",
-	"Symbols Nerd Font Mono",
-	"Noto Color Emoji",
-})
-
-config.font_size = 14
-
 config.default_prog = { "/usr/bin/fish" }
 
-config.colors = {
-	-- Source: https://github.com/scottmckendry/cyberdream.nvim
-	foreground = "#ffffff",
-	background = "#16181a",
+config.font = wezterm.font_with_fallback({
+  "FantasqueSansM Nerd Font",
+  "Symbols Nerd Font Mono",
+  "Noto Color Emoji",
 
-	cursor_bg = "#ffffff",
-	cursor_fg = "#16181a",
-	cursor_border = "#ffffff",
+  harfbuzz_features = { "calt=1", "clig=1", "liga=1" },
+})
 
-	selection_fg = "#ffffff",
-	selection_bg = "#3c4048",
-
-	scrollbar_thumb = "#16181a",
-	split = "#16181a",
-
-	ansi = { "#16181a", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
-	brights = { "#3c4048", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
-	indexed = { [16] = "#ffbd5e", [17] = "#ff6e5e" },
-}
-
-config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
-}
+config.font_size = 16
 
 config.window_background_opacity = 0.8
 
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
+
+config.colors = {
+  -- Source: https://github.com/scottmckendry/cyberdream.nvim
+  foreground = "#ffffff",
+  background = "#16181a",
+
+  cursor_bg = "#ffffff",
+  cursor_fg = "#16181a",
+  cursor_border = "#ffffff",
+
+  selection_fg = "#ffffff",
+  selection_bg = "#3c4048",
+
+  scrollbar_thumb = "#16181a",
+  split = "#16181a",
+
+  ansi = { "#16181a", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
+  brights = { "#3c4048", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
+  indexed = { [16] = "#ffbd5e", [17] = "#ff6e5e" },
+}
+
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
 
 return config
--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Neovide config
+if vim.g.neovide then
+  -- Font
+  vim.o.guifont = "FantasqueSansM Nerd Font Mono,Symbols Nerd Font Mono,Noto_Color_Emoji:h12:b"
+  -- Scale
+  vim.g.neovide_scale_factor = 1.5
+  -- Blur (macOS only)
+  -- vim.g.neovide_window_blurred = true
+  -- Transparency
+  vim.g.neovide_transparency = 0.9
+end
